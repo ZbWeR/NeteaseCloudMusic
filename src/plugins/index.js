3340cff94a3344afa1376a93079dce4587435d42
ZbWeR <1641064392@qq.com>
2023-07-13 23:57:43 +0800

    ✨ feat: 批量导入vant组件库

---
+++ src/plugins/index.js
@@ -0,0 +1,17 @@
+
+import { Swipe, SwipeItem, Button, Sticky, Popup } from 'vant';
+
+let vantComponents = [
+    Swipe,
+    SwipeItem,
+    Button,
+    Sticky,
+    Popup,
+
+]
+
+export default function getVant(app) {
+    vantComponents.forEach(item => {
+        return app.use(item);
+    })
+}
