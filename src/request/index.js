5aa6a41d279766ea8e454a0d06ed4c5acaeb4023
ZbWeR <1641064392@qq.com>
2023-07-15 11:39:49 +0800

    🐞 fix: 更换请求方式为https

--- src/request/index.js
+++ src/request/index.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 let service = axios.create({
-    baseURL: 'http://123.249.120.189:3000',
+    // baseURL: 'http://123.249.120.189:3000',
+    baseURL: 'https://netease-cloud-music-api-roan-five.vercel.app/',
     timeout: 5000,
     headers: {
         Accept: "application/json",
